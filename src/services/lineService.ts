d327a9d96c22ba0e7d4b7546777a0ffc95663ae3
hiromuota166 <otahiromu166@gmail.com>
2024-08-11 23:26:13 +0900

    add:flexMessageを試す

--- src/services/lineService.ts
+++ src/services/lineService.ts
@@ -4,75 +4,27 @@ export const handleLineMessage = (events: any) => {
   let replyMessage;
 
   if (events.message.type === "sticker") {
-    replyMessage = [
-      {
-        "type": "template",
-        "altText": "this is a carousel template",
-        "template": {
-          "type": "carousel",
-          "columns": [
+    replyMessage = {
+      "type": "flex",
+      "altText": "this is a flex message",
+      "contents": {
+        "type": "bubble",
+        "body": {
+          "type": "box",
+          "layout": "vertical",
+          "contents": [
             {
-              "thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
-              "imageBackgroundColor": "#FFFFFF",
-              "title": "this is menu",
-              "text": "description",
-              "defaultAction": {
-                "type": "uri",
-                "label": "View detail",
-                "uri": "http://example.com/page/123"
-              },
-              "actions": [
-                {
-                  "type": "postback",
-                  "label": "Buy",
-                  "data": "action=buy&itemid=111"
-                },
-                {
-                  "type": "postback",
-                  "label": "Add to cart",
-                  "data": "action=add&itemid=111"
-                },
-                {
-                  "type": "uri",
-                  "label": "View detail",
-                  "uri": "http://example.com/page/111"
-                }
-              ]
+              "type": "text",
+              "text": "hello"
             },
             {
-              "thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
-              "imageBackgroundColor": "#000000",
-              "title": "this is menu",
-              "text": "description",
-              "defaultAction": {
-                "type": "uri",
-                "label": "View detail",
-                "uri": "http://example.com/page/222"
-              },
-              "actions": [
-                {
-                  "type": "postback",
-                  "label": "Buy",
-                  "data": "action=buy&itemid=222"
-                },
-                {
-                  "type": "postback",
-                  "label": "Add to cart",
-                  "data": "action=add&itemid=222"
-                },
-                {
-                  "type": "uri",
-                  "label": "View detail",
-                  "uri": "http://example.com/page/222"
-                }
-              ]
+              "type": "text",
+              "text": "world"
             }
-          ],
-          "imageAspectRatio": "rectangle",
-          "imageSize": "cover"
+          ]
         }
       }
-    ];
+    }
   } else if (events.message.type === "text") {
     replyMessage = [{
       type: "text",
